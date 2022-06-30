10efc02a9b0da7f12d76c6d10963ea836e67871b
Mzukisi Makaluza <mzukisimakaluza.dev@gmail.com>
2022-06-30 07:08:40 +0200

    complete the finder.sh script

---
+++ finder-app/finder.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]
+then
+    echo "Invalid number of arguments, expected 2 instead got $# arguments"
+    exit 1
+else
+    filesdir=$1
+    searchstr=$2
+
+    if [ -d "$filesdir" ]
+    then
+        FILES="$(grep -lr ${searchstr} ${filesdir} | wc -l)"
+        LINES="$(grep -r ${searchstr} ${filesdir} | wc -l)" # This counts all matching lines [Correct]
+        echo "The number of files are ${FILES} and the number of matching lines are ${LINES}"
+    else
+        echo "No directory named ${filesdir}"
+        exit 1
+    fi
+fi
+exit 0
